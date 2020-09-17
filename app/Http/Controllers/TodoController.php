5735fcbad84abd00495c8a298e2cfdfa0d6b854e
Abner Rodrigues <abnerluis1001@gmail.com>
2020-09-17 19:41:31 -0300

    feat: created Todo controller

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo as Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    public function index()
+    {
+        $todos = Todo::where('is_completed', false)
+            ->orderBy('created_at', 'desc')
+            ->get();
+
+        return $todos->toJson();
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+
+        Todo::create([
+            'title' => $validatedData['title'],
+            'description' => $validatedData['description'],
+        ]);
+
+        return response()->json('To-do created!');
+    }
+
+    public function show($id)
+    {
+        $todo = Todo::find($id);
+
+        return $todo->toJson();
+    }
+
+    public function markAsCompleted(Todo $todo)
+    {
+        $todo->is_completed = true;
+        $todo->update();
+
+        return response()->json('To-do updated!');
+    }
+}
